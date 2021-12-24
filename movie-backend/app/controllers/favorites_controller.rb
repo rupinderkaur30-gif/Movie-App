3519fb0b616724fafb077ab1acd7ae2ada28fdb1
rupinderkaur <rupi063094@gmail.com>
2021-12-24 17:01:58 -0500

    addchangesincreateaction

--- movie-backend/app/controllers/favorites_controller.rb
+++ movie-backend/app/controllers/favorites_controller.rb
@@ -5,8 +5,13 @@ class FavoriteController < ApplicationController
     end
 
     def create
-        favorite = Favorite.create(favorite_params)
-        render json: favorite
+        if Favorite.where(favorite_params).exits?
+            Favorite.where(favorite_params).destroy_all
+            render json: {favorite: false, movie_id: favorite_params(movie_id)}
+        else
+           favorite = Favorite.create(favorite_params)
+           render json: {favorite: true, movie_id: favorite_params(movie_id)}
+        end
     end
 
     private
